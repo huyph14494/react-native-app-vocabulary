606493b46eff99a1b8df96e5c56256374392133b
Huy <huy.phamhong@haravan.com>
2020-04-11 01:39:42 +0700

    update

--- src/ScreenMain.js
+++ src/ScreenMain.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 import {ScrollView} from 'react-native';
 import List from './components/List';
 import Style from './Style';
-import MyFlatList from './components/MyFlatList';
 import Form from './components/Form';
 import Filter from './components/Filter';
+// import MyFlatList from './components/MyFlatList';
 
 export default class ScreenMain extends Component {
   render() {
